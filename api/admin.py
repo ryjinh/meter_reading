6e195f6e7efdd7cb0b3af9cb2b8bf578081abb26
ryan.healy <ryan.healy@revolut.com>
2022-09-20 15:32:39 +0900

    admin site model + search functionality on mpan/meter id

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+import api.models
 
-# Register your models here.
+
+@admin.register(api.models.Reading)
+class MeterReadingAdmin(admin.ModelAdmin):
+    list_display = (
+        "mpan_core",
+        "register_id",
+        "meter_id",
+        "reading_value",
+        "reading_taken_at",
+        "reading_flag",
+        "reading_method",
+        "file_name",
+    )
+    search_fields = (
+        "mpan_core__mpan_core__startswith",
+        "meter_id__id__startswith",
+    )
